03f3d09b74554d03d421345b1d4c26917b089b82
Eddiexian <43458446+Eddiexian@users.noreply.github.com>
2024-04-10 10:47:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,18 +11,20 @@ connected_clients = set()
 
 
 async def echo(websocket, path):
-    # 將新連接的WebSocket物件加入容器
+    # 将新连接的WebSocket对象加入容器
     connected_clients.add(websocket)
     print(f"New connection from: {websocket.remote_address}", file=sys.stdout)
 
     try:
         async for message in websocket:
-            # 使用遍歷迴圈向每個WebSocket物件傳送訊息（廣播）
+            # 使用循环向每个WebSocket对象发送消息（广播）
             for client in connected_clients:
                 await client.send(message)
     finally:
-        # 連線結束時，從容器中移除相應的WebSocket物件
+        # 从容器中移除相应的WebSocket对象
         connected_clients.remove(websocket)
+        # 发送关闭帧以结束连接
+        await websocket.close()
 
 
 async def health_check(path, request_headers):
